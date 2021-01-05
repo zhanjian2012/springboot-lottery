2ae77207e63898ff1492d311236169c4766588da
zhanjian <746147675@qq.com>
2021-01-05 18:10:20 +0800

    添加抽奖逻辑

--- src/main/java/com/springboot/lottery/util/LotteryUtil.java
+++ src/main/java/com/springboot/lottery/util/LotteryUtil.java
@@ -10,41 +10,6 @@ import java.util.List;
 @Slf4j
 public class LotteryUtil {
 
-//    public static void main(String[] args) {
-//
-//        List<PrizeEntity> data1 = new ArrayList<>();
-//        for (int i = 1; i <= 6; i++) {
-//
-//
-//            PrizeEntity prizeEntity = new PrizeEntity();
-//            prizeEntity.setId(i);
-//            prizeEntity.setPrizeName("奖品" + i);
-//            if (i == 3) {
-//                prizeEntity.setPrizeType(1);
-//                prizeEntity.setPercentage(i * 10);
-//            } else {
-//                prizeEntity.setPrizeType(2);
-//                prizeEntity.setPercentage(i);
-//            }
-//            prizeEntity.setSurplusStock(10);
-//            prizeEntity.setPrizeTotal(100);
-//            data1.add(prizeEntity);
-//        }
-//
-//        int max=0;
-//        List<String> data = new ArrayList<>();
-//        for(PrizeEntity prizeEntity : data1) {
-//            for(int i=0; i<prizeEntity.getSurplusStock(); i++) {
-//                max += 1;
-//                data.add(prizeEntity.getId() + "-" + max);
-//            }
-//        }
-//        log.info("data={}, size={}", data, data.size());
-//        Collections.shuffle(data);
-//        log.info("data={}, size={}", data, data.size());
-//
-//    }
-
     public static List<Object> prizeList(List<PrizeEntity> data1) {
         int max = 0;
         List<Object> data = new ArrayList<>();
