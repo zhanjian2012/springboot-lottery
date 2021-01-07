a59574b558c40b287c1be276c271dd0c94011663
zhanjian <746147675@qq.com>
2021-01-07 16:00:03 +0800

    添加抽奖逻辑

--- src/main/java/com/springboot/lottery/core/runner/LoadPrizeInfoRunner.java
+++ src/main/java/com/springboot/lottery/core/runner/LoadPrizeInfoRunner.java
@@ -43,7 +43,7 @@ public class LoadPrizeInfoRunner implements CommandLineRunner {
                 prizeEntity.setPrizeType(2);
                 prizeEntity.setPercentage(i);
             }
-            prizeEntity.setSurplusStock(i * 10);
+            prizeEntity.setSurplusStock(i * 100);
             list.add(prizeEntity);
         }
         return list;
